55473793a8c568f8be391f37dc370e99dfa22eae
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-04-08 17:09:22 +0200

    Act 4

--- install-repo.sh
+++ install-repo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "🔹 Instalando StormOS Repo..."
+echo "🔹 Instalando StormOS Repo (amd64)..."
 
 # 1. Crear carpeta para keyrings si no existe
 sudo mkdir -p /etc/apt/keyrings
@@ -10,10 +10,10 @@ sudo mkdir -p /etc/apt/keyrings
 # 2. Descargar la clave pública
 curl -fsSL https://raw.githubusercontent.com/acierto-incomodo/stormstore-linux-apt/main/public.key | sudo gpg --dearmor -o /etc/apt/keyrings/stormos.gpg
 
-# 3. Crear archivo .list
-echo "deb [signed-by=/etc/apt/keyrings/stormos.gpg] https://acierto-incomodo.github.io/stormstore-linux-apt stable main" | sudo tee /etc/apt/sources.list.d/stormos.list
+# 3. Crear archivo .list con arch=amd64
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/stormos.gpg] https://acierto-incomodo.github.io/stormstore-linux-apt stable main" | sudo tee /etc/apt/sources.list.d/stormos.list
 
 # 4. Actualizar APT
 sudo apt update
 
-echo "✅ Repo StormOS agregado. Ahora puedes instalar paquetes con: sudo apt install nombre-paquete"
+echo "✅ Repo StormOS (amd64) agregado. Ahora puedes instalar paquetes con: sudo apt install nombre-paquete"
